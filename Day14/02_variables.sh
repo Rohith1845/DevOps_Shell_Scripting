4175cbcd421af2cd10eff8f13ac4e24bee2440f4
rohith <krishnamsettyrohith@gmail.com>
2025-09-20 16:42:38 +0530

    variables committed success

---
+++ Day14/02_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=anil
+person2=rohith
+
+echo "$preson1:: welcome to world $person2"
+echo "$person2:: thank you $person1 finally we meet"
+echo "$person1:: how was your life $person2"
+echo "$person2:: all is well $person1"
