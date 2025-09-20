74a7a2c6b4f5ae968c779c9207e16188b3ce7473
rohith <krishnamsettyrohith@gmail.com>
2025-09-20 16:52:59 +0530

    second

--- Day14/02_variables.sh
+++ Day14/02_variables.sh
@@ -3,7 +3,7 @@
 person1=anil
 person2=rohith
 
-echo "$preson1:: welcome to world $person2"
+echo "$person1:: welcome to world $person2"
 echo "$person2:: thank you $person1 finally we meet"
 echo "$person1:: how was your life $person2"
 echo "$person2:: all is well $person1"
