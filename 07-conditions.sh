410fce3d784df34608ab6ff0d5976921187b8704
rohith <krishnamsettyrohith@gmail.com>
2025-09-23 15:07:29 +0530

    Success

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is less than 10"
-elif[ $NUMBER -ne 10 ]; then
+elif [ $NUMBER -ne 10 ]; then
     echo " Given number $NUMBER is not equals to 10"
 else 
     echo "Given number $NUMBER is equals to 10"
