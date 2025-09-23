98335875d62eeedad8d3015e58bbeaff6f0ddc55
rohith <krishnamsettyrohith@gmail.com>
2025-09-23 14:45:39 +0530

    Success

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,8 @@
 
 NUMBER=$1
 
-if [$NUMBER -lt 10]; then
-
-    echo "Given number is $NUMBER less than 10"
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number $NUMBER is less than 10"
 else
-    echo "Given number is $NUMBER greater tha 10"
+    echo "Given number $NUMBER is greater than 10"
 fi
