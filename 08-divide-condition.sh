199a4ea6ebc757e2d7cf330d6cf0528860741c66
rohith <krishnamsettyrohith@gmail.com>
2025-09-23 15:31:25 +0530

    Success

--- 08-divide-condition.sh
+++ 08-divide-condition.sh
@@ -3,7 +3,7 @@
 echo "Enter the number"
 read NUMBER
 
-if [ $(($NUMBER%2) -eq 0 )]; then
+if [ $(($NUMBER%2)) -eq 0 ]; then
     echo "The number $NUMBER is Even"
 else 
     echo "The number $NUMBER is Odd"
